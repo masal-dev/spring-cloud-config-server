d325360c3c0ce6539b7b20fe74338a037d708add
Marco Salernitano <masal.weblogins@gmail.com>
2024-09-13 19:27:41 +0200

    Configure Config server for local repo
    Access example file application.yml at localhost:8888/application/default

--- src/test/java/it/salernitano/spring_cloud_config_server/SpringCloudConfigServerApplicationTests.java
+++ src/test/java/it/salernitano/spring_cloud_config_server/SpringCloudConfigServerApplicationTests.java
@@ -2,7 +2,6 @@ package it.salernitano.spring_cloud_config_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootTest
 class SpringCloudConfigServerApplicationTests {
